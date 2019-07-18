792234ccb7e84ed628a778201e9ac73ad4b53fd4
Gonzalo Melo <gonzalo@kona.tech>
2019-07-18 10:13:52 -0300

    Use bootMeanDF

--- app/plumber.R
+++ app/plumber.R
@@ -1,5 +1,11 @@
+library(openair)
+
 #' @get /hello
 #' @html
 function(){
-  "<html><h1>hello world</h1></html>"
+  # test <- rnorm(20, mean = 10)
+  test <- c(1,2,3,4,5,6,7,8,9,10)
+  # paste(test, collapse=" ")
+  result <- bootMeanDF(test)
+  toString(result)
 }
