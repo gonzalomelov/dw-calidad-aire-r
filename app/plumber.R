555bde3c9affbc323ee80877b3ce0e31772e66d1
Gonzalo Melo <gonzalo@kona.tech>
2019-07-18 13:02:30 -0300

    Finish bootMeanDF calculation

--- app/plumber.R
+++ app/plumber.R
@@ -2,10 +2,9 @@ library(openair)
 
 #' @get /hello
 #' @html
-function(){
-  # test <- rnorm(20, mean = 10)
-  test <- c(1,2,3,4,5,6,7,8,9,10)
-  # paste(test, collapse=" ")
-  result <- bootMeanDF(test)
+function(a) {
+  v <- as.numeric(unlist(strsplit(a, split=",")))
+  # paste(v, collapse=" ")
+  result <- bootMeanDF(v)
   toString(result)
 }
